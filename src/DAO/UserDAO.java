7bea4421b9aab6fa98c9e20dbf0adf8ff0223829
WickeyR <149520847+WickeyR@users.noreply.github.com>
2025-04-18 14:31:55 -0700

    Implemented doesUsernameExist and doesEmailExist

--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -3,6 +3,9 @@ package DAO;
 import model.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
 
@@ -40,15 +43,47 @@ public class UserDAO {
      * @return true or false is username is found
      */
     public boolean doesUsernameExist(String username, Connection connection){
+        // sql prompt to check if user exists
+        String sql = "SELECT COUNT(*) FROM users WHERE username = ?";
 
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setString(1, username);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next()){
+                    // If count > 0, username is already in database
+                    int count = rs.getInt(1);
+                    return count > 0;
+                }
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return false;
-
     }
 
 
+    /**
+     * @param email the email to check the database for
+     * @param connection the sql connection
+     * @return true or false based on the result
+     * Checks to see if the email already exists in the database
+     */
     public boolean doesEmailExist(String email, Connection connection){
+        // sql prompt to check if user email exists
+        String sql = "SELECT COUNT(*) FROM users WHERE email = ?";
 
-
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setString(1, email);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next()){
+                    // If count > 0, username is already in database
+                    int count = rs.getInt(1);
+                    return count > 0;
+                }
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return false;
     }
 }
